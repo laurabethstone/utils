d89518da0ae51db3eb9b96e40a6e68d179b2e234
Laura Stone <60003873+laurabethstone@users.noreply.github.com>
2022-01-24 16:17:29 -0500

    Update statename_statecode_swap.py

--- statename_statecode_swap.py
+++ statename_statecode_swap.py
@@ -1,1 +1,83 @@
+# __all__ = ["","",""]
 
+import pandas as pd
+import re
+df = pd.DataFrame([''])
+states = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+}
+
+def best_match(x):
+    if len(x) == 2: # Try another way for 2-letter codes
+        for a,n in states.items():
+            if len(n.split()) == 2:
+                if "".join([c[0] for c in n.split()]).lower() == x.lower():
+                    return a.lower()
+    new_rx = re.compile(r"\w*".join([ch for ch in x]), re.I)
+    for a,n in states.items():
+        if new_rx.match(n):
+            return a.lower()
+        
+df['state_corrected'] = df['state'].apply(lambda x: best_match(x))
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
